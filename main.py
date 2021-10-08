4cd148b3e39913f1301b758f0f1755c4d29845d1
Khilnani, Rohan <rohan.khilnani@optum.com>
2021-10-08 23:45:36 +0530

    remove redundant imports

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from fastapi import FastAPI, Form, File, UploadFile
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from typer import Typer
 
 from auto_ml.main import router as auto_ml_router
 from auto_ml.automl import train_model
 
-from fastapi.responses import StreamingResponse
-from typing import Optional
-from io import BytesIO, StringIO
-import pandas as pd
-from loguru import logger
 
 app = FastAPI()
 app.include_router(auto_ml_router, prefix="/auto_ml")
